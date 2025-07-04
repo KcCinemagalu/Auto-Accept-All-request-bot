6b5ca46c02fd9d916f870bfd68e5bbc3501a63c9
Vaibhav Gupta <128729218+vaibhav200420@users.noreply.github.com>
2025-07-05 00:49:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ ADMINS = int(environ.get("ADMINS", "7072018503"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "mongodb+srv://max20:max20@cluster0.y6vu6gp.mongodb.net/?retryWrites=true&w=majority") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = environ.get("DB_NAME", "cluster0")
+DB_NAME = environ.get("DB_NAME", "AcceptAllRequestBot")
 
 # If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
